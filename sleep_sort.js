ad045ec745b9b5aa697a819e44f6b4de0b58a328
Aidan Harding <aidan.harding@nebulaconsulting.co.uk>
2021-06-16 11:55:00 +0100

    Use Promise.all to wait for all promises to resolve

--- sleep_sort.js
+++ sleep_sort.js
@@ -2,16 +2,13 @@ function sleeper(value) {
     return new Promise(resolve => setTimeout(() => resolve(value), value * 100));
 }
 
-async function sleepSort(values) {
+async function sleepSort(toSort) {
     const result = [];
-    return new Promise(
-        resolve =>
-        values.forEach((value) => sleeper(value).then((value) => {console.log(value); result.push(value); if(values.length === result.length) resolve(result);}))
-    );
+    return Promise.all(toSort.map(value => sleeper(value).then(() => result.push(value))))
+        .then(() => result);
 }
 
 (async() => {
     const result = await sleepSort([5, 4, 3, 6, 1]);
-    console.log('done');
     console.log(result);
 })();
