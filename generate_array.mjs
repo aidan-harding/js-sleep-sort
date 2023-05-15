72889c1ca32dfb25bd677f0418ca2f42e087f495
Aidan Harding <aidan.harding@nebulaconsulting.co.uk>
2023-05-15 16:33:34 +0100

    Modified sleep_sort.js to make it demo better

---
+++ generate_array.mjs
@@ -0,0 +1,5 @@
+export default function* generateArray(NUMBER_OF_ITEMS, MAX_VALUE) {
+    for(let i=0; i < NUMBER_OF_ITEMS; i++) {
+        yield Math.round(Math.random() * MAX_VALUE);
+    }
+}
--- sleep_sort.js
+++ sleep_sort.js
@@ -1,14 +1,27 @@
+import generateArray from './generate_array.mjs'
+
+const SLEEP_MULTIPLE = 200;
+const MAX_VALUE = 50;
+const NUMBER_OF_ITEMS = 50;
+
 function sleeper(value) {
-    return new Promise(resolve => setTimeout(() => resolve(value), value * 100));
+    return new Promise(resolve => setTimeout(() => resolve(value), value * SLEEP_MULTIPLE));
+}
+
+async function sleepThenAddToResult(result, value) {
+    return sleeper(value).then(() => {
+        result.push(value);
+        console.error(...result);
+    });
 }
 
 async function sleepSort(toSort) {
     const result = [];
-    return Promise.all(toSort.map(value => sleeper(value).then(() => result.push(value))))
+    return Promise.all(toSort.map(value => sleepThenAddToResult(result, value)))
         .then(() => result);
 }
 
 (async() => {
-    const result = await sleepSort([5, 4, 3, 6, 1]);
-    console.log(result);
+    const result = await sleepSort(Array.from(generateArray(NUMBER_OF_ITEMS, MAX_VALUE)));
+    console.log(...result);
 })();
